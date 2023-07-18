0f6973dcae9c1e984d8a9d882c788fe27b6bc14f
Антон Федотов <anthony.fedotos@gmail.com>
2023-07-18 14:23:52 +0600

    add hanhman

--- hangman.py
+++ hangman.py
@@ -40,9 +40,6 @@ def choice_random_word(word_list: list) -> str:
 
 
 def is_russian_char(guess: str) -> bool:
-
-
-
     """
     Проверяет, является ли буква из русского алфавита.
 
@@ -60,39 +57,63 @@ def is_russian_char(guess: str) -> bool:
         return False
     return True
 
+
 def main():
     list_words = downloads_russian_words()
+
+
+    word_to_guess = choice_random_word(list_words)
 
     list_word_to_guess = list(word_to_guess)
 
     user_word = str()
 
-    word_to_guess = 12
+    guess_number = 12
 
     hidden_word = ""
-    list_of_tried_letters = ""
 
-    for i in word_to_guess: 
+    list_of_tried_letters = ""
+    list_of_correct_tried_letters = ""
+
+    for i in word_to_guess:
         hidden_word += "_"
 
-    while(guess_number !=0):
-        print(hidden_word)
+    list_hidden_word = list(hidden_word)
+
+    while(guess_number != 0):
+        print(list_hidden_word)
+        print(f"Tried letters: {list_of_tried_letters}")
         print(f"Attempts:{guess_number} ")
         print("Letter: ")
 
         current_letter = input()
         if(is_russian_char(current_letter)):
-            if current_letter in list_word_to_guess:
-                # hidden_word.replace("_", current_letter)
-            for i in list_word_to_guess:
-                if (i == current_letter):
-                    list_hidden_word[i] = current_letter
+            if current_letter in list_of_correct_tried_letters:
+                print("You tried this letter it was correct")
+            elif current_letter in list_of_tried_letters:
+                print("You tried this letter it was incorrect")
+            elif current_letter in list_word_to_guess:
+                # hidden_word = hidden_word.replace("_", current_letter)
+                for i, j  in enumerate(list_word_to_guess):
+                    if(j == current_letter):
+                        list_hidden_word[i] = current_letter
+                        list_of_correct_tried_letters += j
             else:
                 guess_number = guess_number - 1
-                list_of_tried_letters +=current_letter
+                list_of_tried_letters += current_letter
         else:
-            print("Enter russain letter")
+            print("Enter russian letter")
 
+        if(list_hidden_word == list_word_to_guess):
+            print("Well done")
+            break
+
+
+
+
+        
+
+        
 
 if __name__ == "__main__":
     main()
